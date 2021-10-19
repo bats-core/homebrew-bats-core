7f9ce70f441ba89e404fe4a2194932bda26e3226
michaeldimmitt <michaelgdimmitt@gmail.com>
2021-10-19 10:07:48 -0400

    add bats-support

---
+++ Formula/bats-support.rb
@@ -0,0 +1,31 @@
+class BatsSupport < Formula
+    desc "Supporting library for Bats test helpers"
+    homepage "https://github.com/bats-core/homebrew-bats-core"
+    url "https://github.com/bats-core/bats-support/archive/refs/tags/v0.3.0.tar.gz"
+    head "https://github.com/bats-core/bats-support.git"
+    version "v0.3.0"
+    sha256 ""
+    license "CC0 1.0 Universal"
+
+    def install
+      mkdir "bats-support"
+      mv "load.bash", "bats-support/"
+      mv "src", "bats-support/"
+      mv "test", "bats-support/"
+      lib.install "bats-support"
+    end
+
+    def caveats
+      <<~EOS
+        To load the bats-support lib in your bats test:
+            load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
+            load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
+      EOS
+    end
+
+    test do
+      ENV["TEST_DEPS_DIR"] = "#{HOMEBREW_PREFIX}/lib"
+      system "bats", "#{lib}/bats-support/test"
+    end
+end
+
