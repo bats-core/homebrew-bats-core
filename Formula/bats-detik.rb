a113fc01f8f86f758f8884d2fc5294957a950c23
michaeldimmitt <michaelgdimmitt@gmail.com>
2021-10-19 10:07:35 -0400

    add bats detik

---
+++ Formula/bats-detik.rb
@@ -0,0 +1,30 @@
+class BatsDetik < Formula
+    desc "A library to ease e2e tests of applications in K8s environments"
+    homepage "https://github.com/bats-core/bats-detik"
+    url "https://github.com/bats-core/bats-detik/archive/refs/tags/v1.1.0.tar.gz"
+    head "https://github.com/bats-core/bats-detik.git"
+    version "v1.1.0"
+    sha256 ""
+    license "CC0 1.0 Universal"
+
+    def install
+      mkdir "bats-detik"
+      mv "load.bash", "bats-detik/"
+      mv "src", "bats-detik/"
+      mv "test", "bats-detik/"
+      lib.install "bats-detik"
+    end
+
+    def caveats
+      <<~EOS
+        To load the bats-detik lib in your bats test:
+            load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
+            load '#{HOMEBREW_PREFIX}/lib/bats-detik/load.bash'
+      EOS
+    end
+
+    test do
+      ENV["TEST_DEPS_DIR"] = "#{HOMEBREW_PREFIX}/lib"
+      system "bats", "#{lib}/bats-detik/test"
+    end
+end
