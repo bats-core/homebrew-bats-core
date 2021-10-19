10b5f7fd55298610262fd6b67756a3ec67ffe72a
michaeldimmitt <michaelgdimmitt@gmail.com>
2021-10-19 10:54:10 -0400

    update commands for detik

--- Formula/bats-detik.rb
+++ Formula/bats-detik.rb
@@ -6,20 +6,22 @@ class BatsDetik < Formula
     version "v1.1.0"
     sha256 "1467f5f1d51211a0cb163cfceee65796e710fe8b3f4b6731d404eb075e9147e3"
     license "CC0 1.0 Universal"
-
     def install
       mkdir "bats-detik"
-      mv "load.bash", "bats-detik/"
-      mv "src", "bats-detik/"
-      mv "test", "bats-detik/"
+      mv "lib", "bats-detik/"
+      mv "examples", "bats-detik/"
+      mv "tests", "bats-detik/"
+      mv "Dockerfile", "bats-detik/"
       lib.install "bats-detik"
     end
 
     def caveats
       <<~EOS
         To load the bats-detik lib in your bats test:
-            load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
-            load '#{HOMEBREW_PREFIX}/lib/bats-detik/load.bash'
+            load '#{HOMEBREW_PREFIX}/lib/bats-support/lib/utils'
+            load '#{HOMEBREW_PREFIX}/lib/bats-detik/lib/detik'
+
+            DETIK_CLIENT_NAME="kubectl"
       EOS
     end
 
