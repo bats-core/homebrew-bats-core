1fad9281886459fd94a28c4ed058aca45f09d5a7
michaeldimmitt <michael.dimmitt@petco.com>
2021-11-03 21:35:12 -0400

    fix brew audits

--- Formula/bats-detik.rb
+++ Formula/bats-detik.rb
@@ -1,32 +1,33 @@
 class BatsDetik < Formula
-    desc "A library to ease e2e tests of applications in K8s environments"
-    homepage "https://github.com/bats-core/bats-detik"
-    url "https://github.com/bats-core/bats-detik/archive/refs/tags/v1.1.0.tar.gz"
-    head "https://github.com/bats-core/bats-detik.git"
-    version "v1.1.0"
-    sha256 "1467f5f1d51211a0cb163cfceee65796e710fe8b3f4b6731d404eb075e9147e3"
-    license "CC0 1.0 Universal"
-    def install
-      mkdir "bats-detik"
-      mv "lib", "bats-detik/"
-      mv "examples", "bats-detik/"
-      mv "tests", "bats-detik/"
-      mv "Dockerfile", "bats-detik/"
-      lib.install "bats-detik"
-    end
+  desc "Library to ease e2e tests of applications in K8s environments"
+  homepage "https://github.com/bats-core/bats-detik"
+  url "https://github.com/bats-core/bats-detik/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "1467f5f1d51211a0cb163cfceee65796e710fe8b3f4b6731d404eb075e9147e3"
+  license "MIT"
+  head "https://github.com/bats-core/bats-detik.git", branch: "master"
 
-    def caveats
-      <<~EOS
-        To load the bats-detik lib in your bats test:
-            load '#{HOMEBREW_PREFIX}/lib/bats-support/lib/utils'
-            load '#{HOMEBREW_PREFIX}/lib/bats-detik/lib/detik'
+  def install
+    mkdir "bats-detik"
+    mv "lib", "bats-detik/"
+    mv "examples", "bats-detik/"
+    mv "tests", "bats-detik/"
+    mv "Dockerfile", "bats-detik/"
+    lib.install "bats-detik"
+  end
 
-            DETIK_CLIENT_NAME="kubectl"
-      EOS
-    end
+  def caveats
+    <<~EOS
+      To load the bats-detik lib in your bats test:
 
-    test do
-      ENV["TEST_DEPS_DIR"] = "#{HOMEBREW_PREFIX}/lib"
-      system "bats", "#{lib}/bats-detik/test"
-    end
+          load '#{HOMEBREW_PREFIX}/lib/bats-detik/lib/utils'
+          load '#{HOMEBREW_PREFIX}/lib/bats-detik/lib/detik'
+
+          DETIK_CLIENT_NAME="kubectl"
+    EOS
+  end
+
+  test do
+    ENV["TEST_DEPS_DIR"] = "#{HOMEBREW_PREFIX}/lib"
+    system "bats", "#{lib}/bats-detik/tests"
+  end
 end
--- Formula/bats-file.rb
+++ Formula/bats-file.rb
@@ -1,31 +1,31 @@
 class BatsFile < Formula
-    desc "Common filesystem assertions for Bats"
-    homepage "https://github.com/bats-core/bats-file"
-    url "https://github.com/bats-core/bats-file/archive/refs/tags/v0.3.0.tar.gz"
-    head "https://github.com/bats-core/bats-file.git"
-    version "v0.3.0"
-    sha256 "54d1c8b1908608aef8e46ac17d4001a75119e279662aeac4e5859ce780515362"
-    license "CC0 1.0 Universal"
+  desc "Common filesystem assertions for Bats"
+  homepage "https://github.com/bats-core/bats-file"
+  url "https://github.com/bats-core/bats-file/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "54d1c8b1908608aef8e46ac17d4001a75119e279662aeac4e5859ce780515362"
+  license "MIT"
+  head "https://github.com/bats-core/bats-file.git", branch: "master"
 
-    def install
-      mkdir "bats-file"
-      mv "load.bash", "bats-file/"
-      mv "src", "bats-file/"
-      mv "test", "bats-file/"
-      lib.install "bats-file"
-    end
+  def install
+    mkdir "bats-file"
+    mv "load.bash", "bats-file/"
+    mv "src", "bats-file/"
+    mv "test", "bats-file/"
+    lib.install "bats-file"
+  end
 
-    def caveats
-      <<~EOS
-        To load the bats-file lib in your bats test:
-            load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
-            load '#{HOMEBREW_PREFIX}/lib/bats-file/load.bash'
-      EOS
-    end
+  def caveats
+    <<~EOS
 
-    test do
-      ENV["TEST_DEPS_DIR"] = "#{HOMEBREW_PREFIX}/lib"
-      system "bats", "#{lib}/bats-file/test"
-    end
+      To load the bats-file lib in your bats test:
+
+          load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
+          load '#{HOMEBREW_PREFIX}/lib/bats-file/load.bash'
+    EOS
+  end
+
+  test do
+    ENV["TEST_DEPS_DIR"] = "#{HOMEBREW_PREFIX}/lib"
+    system "bats", "#{lib}/bats-file/test"
+  end
 end
-
