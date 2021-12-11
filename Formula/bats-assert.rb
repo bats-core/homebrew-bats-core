0c6dac9866714702dc9f14ffa745f1b78e2e6cbd
brokenpip3 <brokenpip3@gmail.com>
2021-12-12 00:41:55 +0100

    add assert tests

--- Formula/bats-assert.rb
+++ Formula/bats-assert.rb
@@ -23,7 +23,29 @@ class BatsAssert < Formula
   end
 
   test do
+    (testpath/"test.bats").write <<~EOS
+      setup() {
+        load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
+        load '#{HOMEBREW_PREFIX}/lib/bats-assert/load.bash'
+      }
+
+      @test "assert true" {
+        assert true
+      }
+
+      @test "refute false" {
+        refute false
+      }
+
+      @test "assert equal" {
+        assert_equal '42' '42'
+      }
+
+      @test "assert not_equal" {
+        assert_not_equal '42' '1'
+      }
+    EOS
     ENV["TEST_DEPS_DIR"] = "#{HOMEBREW_PREFIX}/lib"
-    system "bats", "#{lib}/bats-assert/test"
+    system "bats", "testpath/test.bats"
   end
 end
