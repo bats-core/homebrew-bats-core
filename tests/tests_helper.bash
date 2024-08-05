b187083553d955757b50b9b989bd48e341f6b659
brokenpip3 <brokenpip3@gmail.com>
2024-08-05 23:47:48 +0200

    update detik to 1.3.2, add pre-commit, add tests, add dependabot, add macos matrix

---
+++ tests/tests_helper.bash
@@ -0,0 +1,8 @@
+_tests_helper() {
+    _BREW_PREFIX="$(brew --prefix)"
+    export BATS_LIB_PATH="${BATS_LIB_PATH}:${_BREW_PREFIX}/lib"
+    bats_load_library bats-support
+    bats_load_library bats-assert
+    bats_load_library bats-file
+    bats_load_library bats-detik/detik.bash
+}
